68a4ce12632c49cda38327b074909625a67f223f
mtosity <mtosity@gmail.com>
2020-10-06 16:57:01 +0700

    gooogle ana

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,8 +18,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "UA-179750986-1",
+        head: true,
+        anonymize: true,
+        respectDNT: true,
+        pageTransitionDeplay: 0,
       },
     },
     {
