ed76da6541f89a99374a1f79eb61e5b075d46d69
mtosity <mtosity@gmail.com>
2020-10-12 21:26:27 +0700

    update 12/10, cafe

--- content/posts/2020-10-11-các-loại-cafe-ngon-và-phổ-biến-hiện-nay.md
+++ content/posts/2020-10-11-các-loại-cafe-ngon-và-phổ-biến-hiện-nay.md
@@ -2,12 +2,101 @@
 template: post
 title: Các loại cafe ngon và phổ biến hiện nay
 slug: cac-loai-cafe-ngon-va-pho-bien-hien-nay
-draft: true
+draft: false
 date: 2020-10-11T03:35:26.887Z
 description: Nhiều lúc ra quán cafe toàn kiêu bạc xỉu chả biết các loại cafe còn
   lại là gì cay vãi, sinh ra con blog này để mai mốt order cho sang lên
 category: Life
 tags:
   - Life
+  - Coffee
+  - Cafe
 ---
-d
+
+Tự nhận mình là coffeeholic nhưng khi ra quán toàn: "Em ơi cho một ly cafe sữa", vì cafe đen quá đắng và bị "lú" khi gọi tên những loại cafe **Fancy** kia :<
+
+![inthienhang-menu-cafe](/media/inthienhang-menu-cafe.jpg)
+> Menu cafe đọc vào là lú (inthienhang)
+
+Những loại cafe nổi tiếng hiện nay, đa số bọn nó là của Ý và được pha bằng máy:
+
+# Espresso
+
+`Nén bột Cafe nhiễn cùng với nước nóng`
+
+![coffeebros-espresso](/media/bros-espresso.png)
+
+> Image Source: [coffeebros](https://coffeebros.com/blog/espresso-crema/)
+
+Cà phê Espresso xuất phát từ nước Ý vào đầu những năm 90. Espresso có nghĩa là "một cách tức khắc" - tức là loại cafe có thể phục vụ cho khách hàng ngay lập tức
+
+Được pha chế bằng cách dùng nước nóng nén với áp xuất cao bột cafe được xay mịn (Đa số là bằng máy). Sau khi pha chế trên mặt có lớp bột màu nâu gọi là lớp crema, đóng vai trò trong việc tạo hương thơm. Éo có crema thì Espresso dởm rùi :>
+
+Một ly Espresso chính hiệu sẽ có vị rất đậm và trên mặt có một lớp crema thơm mà không đắng. Cafe Espresso thường được uống bằng tách dày được làm nóng trước, có thể có hoặc không pha thêm đường và thường được phục vụ kèm theo 1 mẩu bánh quy cùng 1 ly nước.
+
+`youtube: https://www.youtube.com/watch?v=5yEtWkIzg40`
+
+# Cappuccino
+
+`Espresso + sữa nóng (x1) + sữa sủi bọt (x2)`
+
+![napoli-cappuccino](/media/napoli-cappuccino.jpg)
+
+> Image Source: [napoli](https://napoli.vn/tin-tuc/ban-co-biet-su-khac-nhau-giua-cappuccino-macchiato-latte-mocha)
+
+Là một dòng họ của **Espresso**, có thể một phần là đánh sữa để tạo bọt sữa và đổ chung vào với **Espresso**, tuỳ tài nghệ có thể cho ra hình hài đẹp lung linh. 
+
+Quan trọng nhất là bước đánh sữa tạo bọt và làm sao kết hợp sữa và **Espresso** để vẽ nên nghệ thuật :>.
+
+Video bên dưới hướng dẫn các làm **Cappuccino** bài bản nhất
+
+`youtube: https://www.youtube.com/watch?v=gvPetTPFsZM`
+
+# Latte
+
+`Espresso + sữa nóng (x2) + sữa sủi bọt (x1)`
+
+
+Y như **Cappochino** nhưng lượng sữa nóng sẽ gấp đôi sữa bọt thay vì ngược lại như **Cappochino**. Điều này sẽ làm vị của **Latte** ngọt hơn **Cappochino** như không creamy bằng.
+
+Một tách Latte bao giờ cũng ít "bồng bềnh" hơn so với Cappuccino. Và theo đúng chuẩn truyền thống, Cappuccino thường được thưởng thức trong những tách dày được hâm nóng trước trong khi Latte lại được uống trong các chiếc ly khá to.
+
+# Flat White 
+
+`Espresso (x2) + sữa nóng (x1) + sữa sủi bọt (x1)`
+
+Bạn sẽ cảm thấy vị cafe mạnh hơn nhưng cafe của tất cả các loại đều như nhau. Chỉ khác tỷ lệ mà thôi :D
+
+# Mocha
+
+`Espresso + sữa nóng (x1) + sữa sủi bọt (x1) + Chocolate`
+
+Người thưởng thức sẽ được hưởng trọn vẹn cả vị thơm béo của kem tươi và vị ngậy của socola nóng. Nhiều người cũng thường phân biệt đơn giản loại đồ uống này là **Latte** + một ít *socola* nóng. Với lượng cafein rất ít và vị ngọt dịu của kem kết hợp socola, loại đồ uống này rất phù hợp cho giới trẻ.
+
+`youtube: https://www.youtube.com/watch?v=p9jY5clw3P0`
+
+# Americano
+
+`Espresso lượng nước gấp đôi`
+
+![americano-14](/media/americano-14.png)
+
+> Image Source: [Kenh14](https://kenh14.vn/co-8-loai-cafe-thong-dung-trong-cac-quan-hien-nay-ma-ai-cung-kho-long-phan-biet-duoc-nhung-nguoi-sanh-uong-doi-khi-con-nham-lan-20200320121826205.chn)
+
+Loại cafe nghe tên "rất Mỹ" nhưng lại hoàn toàn bắt nguồn từ nước Ý này chính là Espresso nhưng được pha loãng với lượng nước gấp đôi. Nhiều người không hề thích Americano tí nào vì họ cho rằng nó đã phá tan cái chuẩn của Espresso, nhưng lại có những người rất khoái Americano vì nó vừa giữ được hương vị của Espresso nhưng đồng thời cũng hạn chế nhiều tác hại từ cafein.
+
+# Cold Brew!
+
+`Bột cafe ngâm trong nước lạnh và được để lạnh trong 12 tiếng`
+
+![coldbrew-14](/media/coldbrew-14.jpg)
+
+> Image Source: [Kenh14](https://kenh14.vn/co-8-loai-cafe-thong-dung-trong-cac-quan-hien-nay-ma-ai-cung-kho-long-phan-biet-duoc-nhung-nguoi-sanh-uong-doi-khi-con-nham-lan-20200320121826205.chn)
+
+Một loại đồ uống khá phổ biến trong các quán cafe hiện nay chính là Cold Brew. Hiểu nôm na, đây là cafe được pha bằng nước lạnh thay vì dùng nước nóng đun sôi như cách pha cà phê truyền thống. Công thức chung để pha loại này là ngâm bột cafe rang xay trong nước lạnh, sau đó để yên và làm lạnh trong thời gian dài (ít nhất 12 tiếng) để chiết xuất được hết tinh chất cafe. Nếu làm đúng cách và để lạnh đủ thời gian, nước cafe sau khi pha xong đôi khi còn đậm đà hơn cả những cách pha cafe nóng quen thuộc.
+
+## Nguồn tham khảo:
+
+[Hoteljob](https://www.hoteljob.vn/tin-tuc/phan-biet-8-loai-thuc-uong-ca-phe-hot-nhat-hien-nay-barista-can-biet)
+
+[Kenh14](https://kenh14.vn/co-8-loai-cafe-thong-dung-trong-cac-quan-hien-nay-ma-ai-cung-kho-long-phan-biet-duoc-nhung-nguoi-sanh-uong-doi-khi-con-nham-lan-20200320121826205.chn)
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
-'use strict';
-require('dotenv').config();
-const siteConfig = require('./config.js');
-const postCssPlugins = require('./postcss-config.js');
+"use strict";
+require("dotenv").config();
+const siteConfig = require("./config.js");
+const postCssPlugins = require("./postcss-config.js");
 
 module.exports = {
   pathPrefix: siteConfig.pathPrefix,
@@ -16,35 +16,35 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/content`,
-        name: 'pages',
+        name: "pages",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/media`,
-        name: 'media',
+        name: "media",
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'css',
+        name: "css",
         path: `${__dirname}/static/css`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'assets',
+        name: "assets",
         path: `${__dirname}/static`,
       },
     },
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         query: `
           {
@@ -66,7 +66,7 @@ module.exports = {
                 date: edge.node.frontmatter.date,
                 url: site.siteMetadata.site_url + edge.node.fields.slug,
                 guid: site.siteMetadata.site_url + edge.node.fields.slug,
-                custom_elements: [{ 'content:encoded': edge.node.html }],
+                custom_elements: [{ "content:encoded": edge.node.html }],
               })),
             query: `
               {
@@ -93,14 +93,14 @@ module.exports = {
                 }
               }
             `,
-            output: '/rss.xml',
+            output: "/rss.xml",
             title: siteConfig.title,
           },
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: process.env.GA_TRAFIC_KEY,
         head: true,
@@ -108,18 +108,18 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
-          'gatsby-remark-relative-images',
+          "gatsby-remark-relative-images",
           {
-            resolve: 'gatsby-remark-katex',
+            resolve: "gatsby-remark-katex",
             options: {
-              strict: 'ignore',
+              strict: "ignore",
             },
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 960,
               withWebp: true,
@@ -127,29 +127,41 @@ module.exports = {
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
-            options: { wrapperStyle: 'margin-bottom: 1.0725rem' },
+            resolve: "gatsby-remark-responsive-iframe",
+            options: { wrapperStyle: "margin-bottom: 1.0725rem" },
           },
-          'gatsby-remark-autolink-headers',
-          'gatsby-remark-prismjs',
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-smartypants',
-          'gatsby-remark-external-links',
+          {
+            resolve: "gatsby-remark-embed-video",
+            options: {
+              width: 800,
+              ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
+              height: 400, // Optional: Overrides optional.ratio
+              related: false,
+              noIframeBorder: true, //Optional: Disable insertion of <style> border: 0
+              containerClass: "embedVideo-container", //Optional: Custom CSS class for iframe container, for multiple classes separate them by space
+            },
+          },
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-prismjs",
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-smartypants",
+          "gatsby-remark-external-links",
+          "gatsby-remark-responsive-iframe",
         ],
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
-    'gatsby-plugin-netlify',
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-netlify",
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
         modulePath: `${__dirname}/src/cms/index.js`,
       },
     },
-    'gatsby-plugin-offline',
+    "gatsby-plugin-offline",
     {
-      resolve: 'gatsby-plugin-sitemap',
+      resolve: "gatsby-plugin-sitemap",
       options: {
         query: `
           {
@@ -171,31 +183,31 @@ module.exports = {
             }
           }
         `,
-        output: '/sitemap.xml',
+        output: "/sitemap.xml",
         serialize: ({ site, allSitePage }) =>
           allSitePage.edges.map((edge) => ({
             url: site.siteMetadata.siteUrl + edge.node.path,
-            changefreq: 'daily',
+            changefreq: "daily",
             priority: 0.7,
           })),
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: siteConfig.title,
         short_name: siteConfig.title,
-        start_url: '/',
-        background_color: '#FFF',
-        theme_color: '#F7A046',
-        display: 'standalone',
-        icon: 'static/photo.jpg',
+        start_url: "/",
+        background_color: "#FFF",
+        theme_color: "#F7A046",
+        display: "standalone",
+        icon: "static/photo.jpg",
       },
     },
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-react-helmet",
     {
-      resolve: 'gatsby-plugin-sass',
+      resolve: "gatsby-plugin-sass",
       options: {
         postCssPlugins: [...postCssPlugins],
         cssLoaderOptions: {
@@ -205,14 +217,14 @@ module.exports = {
         anonymize: true,
       },
     },
-    'gatsby-plugin-flow',
-    'gatsby-plugin-optimize-svgs',
+    "gatsby-plugin-flow",
+    "gatsby-plugin-optimize-svgs",
     {
-      resolve: 'gatsby-plugin-algolia',
+      resolve: "gatsby-plugin-algolia",
       options: {
         appId: process.env.GATSBY_ALGOLIA_APP_ID,
         apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
-        queries: require('./src/utils/algolia-queries'),
+        queries: require("./src/utils/algolia-queries"),
       },
     },
   ],
