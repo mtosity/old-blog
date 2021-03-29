6542b6897f943ab08fda769077dc2ef41704560c
mtosity <mtosity@gmail.com>
2021-03-29 12:09:16 +0700

    checker fix

---
+++ content/posts/2020-03-26-rest-authentication.md
@@ -0,0 +1,18 @@
+---
+template: post
+slug: sql-vs-nosql-comparision
+draft: true
+socialImage: /media/sql-nosql.png
+title: SQL and NoSQL comparison (EN)
+date: 2021-03-21T15:02:50.837Z
+description: This will show differents between SQL and NoSQL and when to use them.
+category: Database
+tags:
+  - Authentication
+  - REST
+  - JWT
+  - Token
+  -
+---
+
+# JWT authentication in the real world
--- src/components/Sidebar/Switcher/Switcher.js
+++ src/components/Sidebar/Switcher/Switcher.js
@@ -4,7 +4,7 @@ import styles from "./Switcher.module.scss";
 const Switcher = () => {
   const [isDarkModeOn, setIsDarkModeOn] = useState(true);
 
-  function turnDarkMode() {
+  function toggleDarkMode() {
     if (window && document) {
       window.localStorage.setItem("darkMode", JSON.stringify(!isDarkModeOn));
       const bodyEl = document.getElementsByTagName("body")[0];
@@ -18,8 +18,9 @@ const Switcher = () => {
   useEffect(() => {
     if (window) {
       const isDark = JSON.parse(window.localStorage.getItem("darkMode"));
-      console.log(isDark);
-      setIsDarkModeOn(isDark || true);
+      if (isDark !== null) {
+        setIsDarkModeOn(isDark);
+      }
     }
   }, []);
 
@@ -27,10 +28,15 @@ const Switcher = () => {
     <div className={styles["switch__container"]}>
       <span className={styles["switch__text"]}>Night mode</span>
       <label className={styles["switch"]}>
-        <input id="DarkModeSwitcher" type="checkbox" value={!isDarkModeOn} />
+        <input
+          id="DarkModeSwitcher"
+          type="checkbox"
+          checked={isDarkModeOn}
+          value={isDarkModeOn}
+        />
         <span
           className={`${styles["slider"]}`}
-          onClick={() => turnDarkMode()}
+          onClick={() => toggleDarkMode()}
         />
       </label>
     </div>
